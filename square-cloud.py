56b288c83fec08461e0683492e601f7532617e8b
ShienKemuriya <carbuncle.ikemen@proton.me>
2026-05-02 03:47:11 +0900

    Add files via upload

---
+++ square-cloud.py
@@ -0,0 +1,50 @@
+from janome.tokenizer import Tokenizer
+from wordcloud import WordCloud
+import json
+import re
+
+
+# tweets.jsの読み込み
+with open("tweets.js", "r", encoding="utf-8") as f:
+    data = f.read()
+
+# tweets.jsの整形（Json化）
+json_str = re.sub(r"window\.YTD\.tweets\.part0\s*=\s*", "", data)# URLの除外
+json_str = json_str.strip().rstrip(";")
+tweets = json.loads(json_str)
+
+#jsonからテキスト抽出
+texts = []
+
+for t in tweets:
+    tweet = t["tweet"]
+    texts.append(tweet["full_text"])
+
+
+t = Tokenizer()
+s = []
+
+# 除外するワードリスト
+stopwords = {
+    "の","に","は","を","た","が","で","て","と","し","れ","さ",
+    "ある","する","いる","こと","これ","それ","あれ","ん"
+}
+
+for text in texts:
+    text = re.sub(r'https?://\S+', '', text)
+    for token in t.tokenize(text):
+        p = token.part_of_speech.split(",")
+        surface = token.surface
+        if "名詞" in p and surface not in stopwords:
+            s.append(token.surface)
+
+wc = WordCloud(
+            width=640,
+            height=480,
+            #フォントはファイルパスで指定
+            font_path="/usr/share/fonts/opentype/noto/NotoSansCJK-Medium.ttc",
+            )
+
+
+wc.generate(" ".join(s))
+wc.to_file('word-cloud-squea.png')
